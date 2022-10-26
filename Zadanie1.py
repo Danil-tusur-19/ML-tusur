f9067d9e2742f177caec412f2e482d4a26e623e2
Danil-tusur-19 <116442091+Danil-tusur-19@users.noreply.github.com>
2022-10-26 23:56:04 +0700

    Update and rename Zadanie1_Ivanov.py to Zadanie1.py

---
+++ Zadanie1.py
@@ -0,0 +1,3 @@
+name = str(input("Введите ваше имя: "))
+surname = str(input("Введите вашу фамилию: "))
+print(f"Hello {name} {surname}! You just delved into Python. Great start!")
--- Zadanie1_Ivanov.py
+++
@@ -1,4 +0,0 @@
-
-name = input("Введите ваше имя: ")
-surname = input("Введите вашу фамилию: ")
-print(f"Hello {name} {surname}! You just delved into Python. Great start!")
